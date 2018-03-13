5e16005f20e515a3a46bf47b9488265ddb3a3ee3
Mirko Vogel <mirko.vogel@gmail.com>
2018-03-13 09:31:33 +0100

    Adding CollocationCandidateHelpers and login template

---
+++ CollocationCandidateHelpers.py
@@ -0,0 +1,74 @@
+# coding=utf-8
+'''
+Created on Mar 11, 2018
+
+@author: mirko
+'''
+
+import re
+
+#def pretty_print_example($e)
+#set $toks = $e.sentence.split(" ")
+#set $hl_toks = ["<b>%s</b>" % w if n in $e.coll_pos else w for (n, w) in $enumerate($toks)]
+#set hl = $str.join(" ", $hl_toks)
+## Yes, this is embarrasing
+#$re.sub(u" ،", u"،", re.sub(u" [.]", u".", $hl))
+#end def
+
+def get_example(c):
+    """
+    
+    """
+    ic = c.get("inflected_forms", {})
+    ics = sorted(ic.iteritems(), key=lambda (_, ic): ic["freq"],
+                 reverse=True)
+    for (ic_key, ic) in ics:
+        for e in ic["examples"].itervalues():
+            # Check for rating
+            return (ic_key, e)
+    
+    return None, None
+
+
+def get_canonical_form(c):
+    """
+    Returns most frequent form without determination if this form exists, too
+    
+    """
+    ics = sorted(c["inflected_forms"].itervalues(), key = lambda ic: ic["freq"])
+    if not ics:
+            return "%s %s" % (c["core_lemma"], c["coll_lemma"])
+
+    candidates = [(ic["core_word"], ic["coll_word"]) for ic in ics]
+    core, coll = candidates[-1]
+    core_indet = core[2:] if core.startswith(u"ال") else core
+    coll_indet = coll[2:] if coll.startswith(u"ال") else coll
+    
+    if (core_indet, coll_indet) in candidates:
+        return "%s %s" % (core_indet, coll_indet)
+    return "%s %s" % (core, coll)
+
+
+def get_rating(o):
+    """
+    Maps the o["ratings"] dict to a string: "none", "mixed" or a rating"""
+    try:
+        s = set(o["ratings"].itervalues())
+    except:
+        return None
+    
+    if len(s) == 0:
+        return None
+    if len(s) > 1:
+        return "mixed"
+    return next(iter(s))
+
+def pretty_print_example(e):
+    toks = e["sentence"].split(" ")
+    hl_toks = ["<b>%s</b>" % w if n in e["coll_pos"] else w for (n, w) in enumerate(toks)]
+    hl = u" ".join(hl_toks)
+    return re.sub(u" ،", u"،", re.sub(u" [.]", u".", hl))
+
+
+if __name__ == '__main__':
+    pass
