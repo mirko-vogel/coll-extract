0f35d9c56b848c4f74bba91bba3fd116e8a383a2
Mirko Vogel <mirko.vogel@gmail.com>
2018-10-07 19:25:43 +0200

    Displaying collocations for collocations

--- synextraction/CollocationWebInterface.py
+++ synextraction/CollocationWebInterface.py
@@ -12,6 +12,7 @@ import sys, os
 import cherrypy, cherrypy.lib.static
 from collections import defaultdict
 
+import itertools
 from Cheetah.Template import Template
 from bson import ObjectId
 from pyarabic import araby
@@ -32,14 +33,41 @@ class CollocationWebInterface(object):
     def index(self):
         return self.show(u"قَلَّب")
 
+    def get_collocations(self, core_lempos):
+        """
+        Get collocations from db containing the passed lempos.
+
+        :param core_lempos: list of lempos
+        :return: dictionary of (pattern, core_indexes) -> collocations
+
+        """
+        # FIXME? Do not retreive instances, only instance count
+        result = self.collocations.find({"lempos": {"$all": core_lempos}})
+        colls = defaultdict(list)
+        for c in result:
+            # The $all operator returns all collocations containing the lempos specified
+            # in the query regardeless of the order. We want ascending order.
+            core_indexes = tuple(c["lempos"].index(lempos) for lempos in core_lempos)
+            if not all(a < b for (a, b) in zip(core_indexes[:-1], core_indexes[1:])):
+                continue
+
+            # Group collocations not only by pattern, but by the position of the core lempos
+            # in that pattern, e.g. NOUN + noun and noun + NOUN.
+            colls[(c["pattern"], core_indexes)].append(c)
+
+        return colls
+
     @cherrypy.expose
     def show(self, lempos):
-        """Returns html page"""
-        colls = defaultdict(list)
-        for d in self.collocations.find({"lempos": lempos}):
-            core_idx = d["lempos"].index(lempos)
-            colls[(d["pattern"], core_idx)].append(d)
-        params = { "lempos": lempos, "collocations": colls }
+        """
+        :param lempos: space separated sequence of core lempos
+        :return: html page
+
+        """
+        core_lempos = lempos.split(" ")
+        colls = self.get_collocations(core_lempos)
+
+        params = { "lempos": core_lempos, "collocations": colls }
         tmpl = file("../templates/collocations.tmpl").read().decode("utf-8")
         t = Template(tmpl, searchList = [params])
         return unicode(t).encode("utf8")
@@ -47,18 +75,22 @@ class CollocationWebInterface(object):
     @cherrypy.expose
     def search(self, w):
         """Returns html page"""
-        r = self.lemmas.find({"lemma": w}, {"lemma": 1, "_id": 0, "pos": 1})
-        if not r.count():
-            r = self.lemmas.find({"unvocalized_lemma": araby.strip_tashkeel(w)},
-                                 {"lemma": 1, "_id": 0, "pos": 1})
+        candidate_lists = []
+        for word in w.split():
+            r = self.lemmas.find({"lemma": word}, {"lemma": 1, "_id": 0, "pos": 1})
+            if not r.count():
+                r = self.lemmas.find({"unvocalized_lemma": araby.strip_tashkeel(word)},
+                                     {"lemma": 1, "_id": 0, "pos": 1})
 
-        if r.count() == 1:
-            lempos = u"{lemma}+{pos}".format(**next(r))
-            return self.show(lempos)
+            candidate_lists.append([u"{lemma}+{pos}".format(**d) for d in r])
+
+        candidates = tuple(itertools.product(*candidate_lists))
+        if len(candidates) == 1:
+            return self.show(" ".join(candidates[0]))
 
         else:
             tmpl = file("../templates/select_lemma.tmpl").read().decode("utf-8")
-            t = Template(tmpl, searchList=[{"lempos": list(r), "word": w}])
+            t = Template(tmpl, searchList=[{"candidates": candidates, "word": w}])
             return unicode(t).encode("utf8")
 
 
